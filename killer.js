1d253e955457bad5f0038fe9f5ea1a8ed1e8a9c4
Rhyno <r.c83@hotmail.co.uk>
2024-07-15 20:36:40 +0100

    first commit

---
+++ killer.js
@@ -0,0 +1,100 @@
+killerImages = ["./Killer/K01_TheTrapper_Portrait.webp", "./Killer/K02_TheWraith_Portrait.webp", "./Killer/K03_TheHillbilly_Portrait.webp", "./Killer/K04_TheNurse_Portrait.webp", "./Killer/K05_TheShape_Portrait.webp", "./Killer/K06_TheHag_Portrait.webp", "./Killer/K07_TheDoctor_Portrait.webp", "./Killer/K08_TheHuntress_Portrait.webp", "./Killer/K09_TheCannibal_Portrait.webp", "./Killer/K10_TheNightmare_Portrait.webp", "./Killer/K11_ThePig_Portrait.webp", "./Killer/K12_TheClown_Portrait.webp", "./Killer/K13_TheSpirit_Portrait.webp", "./Killer/K14_TheLegion_Portrait.webp", "./Killer/K15_ThePlague_Portrait.webp", "./Killer/K16_TheGhostFace_Portrait.webp", "./Killer/K17_TheDemogorgon_Portrait.webp", "./Killer/K18_TheOni_Portrait.webp", "./Killer/K19_TheDeathslinger_Portrait.webp", "./Killer/K20_TheExecutioner_Portrait.webp", "./Killer/K21_TheBlight_Portrait.webp", "./Killer/K22_TheTwins_Portrait.webp", "./Killer/K23_TheTrickster_Portrait.webp", "./Killer/K24_TheNemesis_Portrait.webp", "./Killer/K25_TheCenobite_Portrait.webp", "./Killer/K26_TheArtist_Portrait.webp", "./Killer/K27_TheOnryo_Portrait.webp", "./Killer/K28_TheDredge_Portrait.webp", "./Killer/K29_TheMastermind_Portrait.webp", "./Killer/K30_TheKnight_Portrait.webp", "./Killer/K31_TheSkullMerchant_Portrait.webp", "./Killer/K32_TheSingularity_Portrait.webp", "./Killer/K33_TheXenomorph_Portrait.webp", "./Killer/K34_TheGoodGuy_Portrait.webp", "./Killer/K35_TheUnknown_Portrait.webp", "./Killer/K36_TheLich_Portrait.webp"];
+killerPerkImages = ["./KillerPerks/agitation.webp", "./KillerPerks/alienInstinct.webp", "./KillerPerks/aNursesCalling.webp", 
+        "./KillerPerks/awakenedAwarenesss.webp", "./KillerPerks/bamboozle.webp", "./KillerPerks/batteriesIncluded.webp",
+        "./KillerPerks/BBQAndChili.webp", "./KillerPerks/BeastOfPrey.webp", "./KillerPerks/bitterMurmur.webp",
+        "./KillerPerks/bloodEcho.webp", "./KillerPerks/bloodhound.webp", "./KillerPerks/bloodWarden.webp",
+        "./KillerPerks/boonDestroyer.webp", "./KillerPerks/brutalStrength.webp", "./KillerPerks/callOfBrine.webp",
+        "./KillerPerks/corruptIntervention.webp", "./KillerPerks/coulrophobia.webp", "./KillerPerks/coupDeGrace.webp",
+        "./KillerPerks/cruelConfinement.webp", "./KillerPerks/darkArrogance.webp", "./KillerPerks/darkDevotion.webp",
+        "./KillerPerks/darknessRevelated.webp", "./KillerPerks/Deadlock.webp", "./KillerPerks/deadManSwitch.webp",
+        "./KillerPerks/deathbound.webp", "./KillerPerks/deerstalker.webp", "./KillerPerks/devourHope.webp",
+        "./KillerPerks/discordance.webp", "./KillerPerks/dissolution.webp", "./KillerPerks/distressing.webp",
+        "./KillerPerks/dragonsGrip.webp", "./KillerPerks/dyingLight.webp", "./KillerPerks/enduring.webp",
+        "./KillerPerks/eruption.webp", "./KillerPerks/fireUp.webp", "./KillerPerks/forcedHesitation.webp",
+        "./KillerPerks/forcedPenance.webp", "./KillerPerks/franklinsLoss.webp", "./KillerPerks/friendsTillTheEnd.webp",
+        "./KillerPerks/furtiveChase.webp", "./KillerPerks/gameAfoot.webp", "./KillerPerks/gearHead.webp",
+        "./KillerPerks/generatorOvercharge.webp", "./KillerPerks/geneticLimits.webp","./KillerPerks/grimEmbrace.webp",
+        "./KillerPerks/hangmansTrick.webp", "./KillerPerks/hatred.webp", "./KillerPerks/hauntedGround.webp",
+        "./KillerPerks/hexBloodFavor.webp", "./KillerPerks/HexCrowdControl.webp", "./KillerPerks/hexFaceTheDarkness.webp",
+        "./KillerPerks/hexPentimento.webp", "./KillerPerks/HexPlaything.webp", "./KillerPerks/hexRetribution.webp",
+        "./KillerPerks/hexUndying.webp", "./KillerPerks/Hoarder.webp", "./KillerPerks/hubris.webp",
+        "./KillerPerks/HuntressLullaby.webp", "./KillerPerks/hysteria.webp", "./KillerPerks/imAllEars.webp",
+        "./KillerPerks/infectiousFright.webp", "./KillerPerks/insidious.webp", "./KillerPerks/ironGrasp.webp",
+        "./KillerPerks/ironMaiden.webp", "./KillerPerks/knockOut.webp", "./KillerPerks/languidTouch.webp",
+        "./KillerPerks/lethalPursuer.webp", "./KillerPerks/leverage.webp", "./KillerPerks/lightborn.webp",
+        "./KillerPerks/madGrit.webp", "./KillerPerks/makeYourChoice.webp", "./KillerPerks/mercilessStorm.webp",
+        "./KillerPerks/mindBreaker.webp", "./KillerPerks/monitorAndAbuse.webp", "./KillerPerks/monstrousShrine.webp",
+        "./KillerPerks/nemesis.webp", "./KillerPerks/noOneEscapesDeath.webp", "./KillerPerks/NoWayOut.webp",
+        "./KillerPerks/nowhereToHide.webp", "./KillerPerks/Oppression.webp", "./KillerPerks/overwhelmingPresence.webp",
+        "./KillerPerks/painResonance.webp", "./KillerPerks/playWithYourFood.webp", "./KillerPerks/popGoesTheWeasel.webp",
+        "./KillerPerks/predator.webp", "./KillerPerks/rapidBrutality.webp", "./KillerPerks/ruin.webp",
+        "./KillerPerks/saveTheBestForLast.webp", "./KillerPerks/ScourgeHookGiftOfPain.webp", "./KillerPerks/selfAware.webp",
+        "./KillerPerks/septicTouch.webp", "./KillerPerks/shadowborn.webp", "./KillerPerks/sloppyButcher.webp",
+        "./KillerPerks/spiesFromTheShadows.webp", "./KillerPerks/spiritFury.webp", "./KillerPerks/Starstruck.webp",
+        "./KillerPerks/stridor.webp", "./KillerPerks/superiorAnatomy.webp", "./KillerPerks/surge.webp",
+        "./KillerPerks/surveillance.webp", "./KillerPerks/terminus.webp", "./KillerPerks/TerritorialImperative.webp",
+        "./KillerPerks/thatanophobia.webp", "./KillerPerks/theThirdSeal.webp", "./KillerPerks/thrillingTremors.webp",
+        "./KillerPerks/thrillOfTheHunt.webp", "./KillerPerks/thwack.webp", "./KillerPerks/tinkerer.webp",
+        "./KillerPerks/trailOfTorment.webp", "./KillerPerks/twoCanPlay.webp", "./KillerPerks/ultimateWeapon.webp",
+        "./KillerPerks/unbound.webp", "./KillerPerks/undone.webp", "./KillerPerks/unforeseen.webp",
+        "./KillerPerks/unnervingPresence.webp", "./KillerPerks/unrelenting.webp", "./KillerPerks/weaveAttunement.webp",
+        "./KillerPerks/whispers.webp", "./KillerPerks/zanshinTactics.webp"
+];
+
+var index = 0;
+var imageElement = document.getElementById("killerImg");
+
+var perk1 = document.getElementById("perk1");
+var perk1text = document.getElementById("perk1text");
+var perk2 = document.getElementById("perk2");
+var perk2text = document.getElementById("perk2text");
+var perk3 = document.getElementById("perk3");
+var perk3text = document.getElementById("perk3text");
+var perk4 = document.getElementById("perk4");
+var perk4text = document.getElementById("perk4text");
+var spinning = false
+
+function changeKillerImage() {
+    index = Math.floor(Math.random()*(killerImages.length-1 - 0 + 1) + 0)
+    imageElement.src = killerImages[index];
+
+}
+
+async function spin() {
+    spinning = !spinning
+    console.log(spinning)
+    
+    var timer = setInterval(changeKillerImage, 40)
+    var perkTimer = setInterval(changePerkImage)
+    
+    await new Promise(r => setTimeout(r, 2000));
+    clearInterval(timer);
+    clearInterval(perkTimer);
+}
+
+function changePerkImage() {
+    var index1 = Math.floor(Math.random()*(killerPerkImages.length-1 - 0 + 1) + 0)
+    var index2 = Math.floor(Math.random()*(killerPerkImages.length-1 - 0 + 1) + 0)
+    while( index2 == index1)
+    {
+        var index2 = Math.floor(Math.random()*(killerPerkImages.length-1 - 0 + 1) + 0)
+    }
+    var index3 = Math.floor(Math.random()*(killerPerkImages.length-1 - 0 + 1) + 0)
+    while (index3 == index2 || index3 == index1)
+    {
+        var index3 = Math.floor(Math.random()*(killerPerkImages.length-1 - 0 + 1) + 0)
+    }
+    var index4 = Math.floor(Math.random()*(killerPerkImages.length-1 - 0 + 1) + 0)
+    while (index4 == index2 || index4 == index2 || index4 == index1)
+    {
+        var index4 = Math.floor(Math.random()*(killerPerkImages.length-1 - 0 + 1) + 0)
+    }
+    perk1.src = killerPerkImages[index1];
+    perk1text.innerHTML = killerPerkImages[index1].substring(14, killerPerkImages[index1].length - 5);
+    perk2.src = killerPerkImages[index2];
+    perk2text.innerHTML = killerPerkImages[index2].substring(14, killerPerkImages[index2].length - 5);
+    perk3.src = killerPerkImages[index3];
+    perk3text.innerHTML = killerPerkImages[index3].substring(14, killerPerkImages[index3].length - 5);
+    perk4.src = killerPerkImages[index4];
+    perk4text.innerHTML = killerPerkImages[index4].substring(14, killerPerkImages[index4].length - 5);
+}
---
+++ survivor.js
@@ -0,0 +1,125 @@
+SurvivorImg = [
+    "./Survivor/S01_DwightFairfield_Portrait.webp", "./Survivor/S02_MegThomas_Portrait.webp", "./Survivor/S03_ClaudetteMorel_Portrait.webp",
+    "./Survivor/S04_JakePark_Portrait.webp", "./Survivor/S06_LaurieStrode_Portrait.webp", "./Survivor/S07_AceVisconti_Portrait.webp", 
+    "./Survivor/S08_BillOverbeck_Portrait.webp", "./Survivor/S09_FengMin_Portrait.webp", "./Survivor/S10_DavidKing_Portrait.webp", 
+    "./Survivor/S11_QuentinSmith_Portrait.webp", "./Survivor/S12_DavidTapp_Portrait.webp", "./Survivor/S13_KateDenson_Portrait.webp", 
+    "./Survivor/S14_AdamFrancis_Portrait.webp", "./Survivor/S15_JeffJohansen_Portrait.webp", "./Survivor/S16_JaneRomero_Portrait.webp", 
+    "./Survivor/S17_AshWilliams_Portrait.webp", "./Survivor/S18_NancyWheeler_Portrait.webp", "./Survivor/S19_SteveHarrington_Portrait.webp", 
+    "./Survivor/S20_YuiKimura_Portrait.webp", "./Survivor/S21_ZarinaKassir_Portrait.webp", "./Survivor/S22_CherylMason_Portrait.webp", 
+    "./Survivor/S23_FelixRichter_Portrait.webp", "./Survivor/S24_ElodieRakoto_Portrait.webp", "./Survivor/S25_Yun-JinLee_Portrait.webp", 
+    "./Survivor/S26_JillValentine_Portrait.webp", "./Survivor/S27_LeonScottKennedy_Portrait.webp", "./Survivor/S28_MikaelaReid_Portrait.webp", 
+    "./Survivor/S29_JonahVasquez_Portrait.webp", "./Survivor/S30_YoichiAsakawa_Portrait.webp", "./Survivor/S31_HaddieKaur_Portrait.webp", 
+    "./Survivor/S32_AdaWong_Portrait.webp", "./Survivor/S33_RebeccaChambers_Portrait.webp", "./Survivor/S34_VittorioToscano_Portrait.webp", 
+    "./Survivor/S35_ThalitaLyra_Portrait.webp", "./Survivor/S36_RenatoLyra_Portrait.webp", "./Survivor/S37_GabrielSoma_Portrait.webp", 
+    "./Survivor/S38_NicolasCage_Portrait.webp", "./Survivor/S39_EllenRipley_Portrait.webp", "./Survivor/S40_AlanWake_Portrait.webp", 
+    "./Survivor/S41_SableWard_Portrait.webp", "./Survivor/S42_TheTroupe_Portrait.webp", "./Survivor/S43_LaraCroft_Portrait.webp"
+];
+
+SurvivorPerkImg = [
+    "./SurvivorPerks/aceInTheHole.webp", "./SurvivorPerks/adrenaline.webp", "./SurvivorPerks/aftercare.webp",
+    "./SurvivorPerks/alert.webp", "./SurvivorPerks/anyMeansNecessary.webp", "./SurvivorPerks/appraisal.webp",
+    "./SurvivorPerks/autodidact.webp", "./SurvivorPerks/babySitter.webp", "./SurvivorPerks/backgroundPlayer.webp",
+    "./SurvivorPerks/balancedLanding.webp", "./SurvivorPerks/bardicInspiration.webp", "./SurvivorPerks/betterThanNew.webp",
+    "./SurvivorPerks/betterTogether.webp", "./SurvivorPerks/BiteTheBullet.webp", "./SurvivorPerks/BlastMine.webp",
+    "./SurvivorPerks/bloodPact.webp", "./SurvivorPerks/bloodRush.webp", "./SurvivorPerks/boilOver.webp",
+    "./SurvivorPerks/bond.webp", "./SurvivorPerks/BoonCircleOfHealing.webp", "./SurvivorPerks/BoonExponential.webp",
+    "./SurvivorPerks/BoonShadowStep.webp", "./SurvivorPerks/borrowedTime.webp", "./SurvivorPerks/botanyKnowledge.webp",
+    "./SurvivorPerks/breakdown.webp", "./SurvivorPerks/breakout.webp", "./SurvivorPerks/buckleUp.webp",
+    "./SurvivorPerks/builtToLast.webp", "./SurvivorPerks/calmSpirit.webp", "./SurvivorPerks/camaraderie.webp",
+    "./SurvivorPerks/championOfLight.webp", "./SurvivorPerks/chemicalTrap.webp", "./SurvivorPerks/Clairvoyance.webp",
+    "./SurvivorPerks/CorrectiveAction.webp", "./SurvivorPerks/Counterforce.webp", "./SurvivorPerks/cutLoose.webp",
+    "./SurvivorPerks/danceWithMe.webp", "./SurvivorPerks/darkSense.webp", "./SurvivorPerks/darkTheory.webp",
+    "./SurvivorPerks/DeadHard.webp", "./SurvivorPerks/deadline.webp", "./SurvivorPerks/deception.webp",
+    "./SurvivorPerks/decisiveStrike.webp", "./SurvivorPerks/dejaVu.webp", "./SurvivorPerks/deliverance.webp",
+    "./SurvivorPerks/desperateMeasures.webp", "./SurvivorPerks/detectivesHunch.webp", "./SurvivorPerks/distortion.webp",
+    "./SurvivorPerks/diversion.webp", "./SurvivorPerks/dramaturgy.webp", "./SurvivorPerks/empathicConnection.webp",
+    "./SurvivorPerks/empathy.webp", "./SurvivorPerks/FastTrack.webp", "./SurvivorPerks/finesse.webp",
+    "./SurvivorPerks/fixated.webp", "./SurvivorPerks/Flashbang.webp", "./SurvivorPerks/flipFlop.webp",
+    "./SurvivorPerks/floodOfRage.webp", "./SurvivorPerks/fogwise.webp", "./SurvivorPerks/forThePeople.webp",
+    "./SurvivorPerks/friendlyCompetition.webp", "./SurvivorPerks/hardened.webp", "./SurvivorPerks/headOn.webp",
+    "./SurvivorPerks/hope.webp", "./SurvivorPerks/hyperfocus.webp", "./SurvivorPerks/illumination.webp",
+    "./SurvivorPerks/innerFocus.webp", "./SurvivorPerks/innerStrength.webp", "./SurvivorPerks/invocationWeavingSpiders.webp",
+    "./SurvivorPerks/ironWill.webp", "./SurvivorPerks/kindred.webp", "./SurvivorPerks/leader.webp",
+    "./SurvivorPerks/leftBehind.webp", "./SurvivorPerks/lightFooted.webp", "./SurvivorPerks/lightweight.webp",
+    "./SurvivorPerks/lithe.webp", "./SurvivorPerks/lowProfile.webp", "./SurvivorPerks/luckyBreak.webp",
+    "./SurvivorPerks/luckyStar.webp", "./SurvivorPerks/madeForThis.webp", "./SurvivorPerks/mettleOfMan.webp",
+    "./SurvivorPerks/mirroredIllusion.webp", "./SurvivorPerks/NoMither.webp", "./SurvivorPerks/noOneLeftBehind.webp",
+    "./SurvivorPerks/objectOfObsession.webp", "./SurvivorPerks/offTheRecord.webp", "./SurvivorPerks/openHanded.webp",
+    "./SurvivorPerks/Overcome.webp", "./SurvivorPerks/overzealous.webp", "./SurvivorPerks/parentalGuidance.webp",
+    "./SurvivorPerks/pharmacy.webp", "./SurvivorPerks/plotTwist.webp", "./SurvivorPerks/plunderersInstinct.webp",
+    "./SurvivorPerks/poised.webp", "./SurvivorPerks/potentialEnergy.webp", "./SurvivorPerks/powerStruggle.webp",
+    "./SurvivorPerks/premonition.webp", "./SurvivorPerks/proveThyself.webp", "./SurvivorPerks/quickAndQuiet.webp",
+    "./SurvivorPerks/reactiveHealing.webp", "./SurvivorPerks/reassurance.webp", "./SurvivorPerks/redHerring.webp",
+    "./SurvivorPerks/rememberMe.webp", "./SurvivorPerks/repressedAlliance.webp", "./SurvivorPerks/residualManifest.webp",
+    "./SurvivorPerks/resilience.webp", "./SurvivorPerks/Resurgence.webp", "./SurvivorPerks/RookieSpirit.webp",
+    "./SurvivorPerks/saboteur.webp", "./SurvivorPerks/scavenger.webp", "./SurvivorPerks/scenePartner.webp",
+    "./SurvivorPerks/secondWind.webp", "./SurvivorPerks/Self-Preservation.webp", "./SurvivorPerks/selfCare.webp",
+    "./SurvivorPerks/slipperyMeat.webp", "./SurvivorPerks/smallGame.webp", "./SurvivorPerks/SmashHit.webp",
+    "./SurvivorPerks/soleSurvivor.webp", "./SurvivorPerks/solidarity.webp", "./SurvivorPerks/soulGuard.webp",
+    "./SurvivorPerks/specialist.webp", "./SurvivorPerks/spineChill.webp", "./SurvivorPerks/sprintBurst.webp",
+    "./SurvivorPerks/stakeOut.webp", "./SurvivorPerks/stillSight.webp", "./SurvivorPerks/streetwise.webp",
+    "./SurvivorPerks/strengthInShadows.webp", "./SurvivorPerks/teamworkCollectiveStealth.webp", "./SurvivorPerks/teamworkPowerOfTwo.webp",
+    "./SurvivorPerks/technician.webp", "./SurvivorPerks/tenacity.webp", "./SurvivorPerks/thisIsNotHappening.webp",
+    "./SurvivorPerks/troubleshooter.webp", "./SurvivorPerks/unbreakable.webp", "./SurvivorPerks/upTheAnte.webp",
+    "./SurvivorPerks/urbanEvasion.webp", "./SurvivorPerks/vigil.webp", "./SurvivorPerks/visionary.webp",
+    "./SurvivorPerks/vittoriosGambit.webp", "./SurvivorPerks/wakeUp.webp", "./SurvivorPerks/wellMakeIt.webp",
+    "./SurvivorPerks/WereGonnaLiveForever.webp", "./SurvivorPerks/wicked.webp", "./SurvivorPerks/windowsOfOpportunity.webp",
+    "./SurvivorPerks/wiretap.webp"
+];
+var index = 0;
+var imageElement = document.getElementById("survivorImg");
+
+var perk1 = document.getElementById("perk1");
+var perk1text = document.getElementById("perk1text");
+var perk2 = document.getElementById("perk2");
+var perk2text = document.getElementById("perk2text");
+var perk3 = document.getElementById("perk3");
+var perk3text = document.getElementById("perk3text");
+var perk4 = document.getElementById("perk4");
+var perk4text = document.getElementById("perk4text");
+var spinning = false
+
+function changeSurvivorImage() {
+    index = Math.floor(Math.random()*(SurvivorImg.length-1 - 0 + 1) + 0)
+    imageElement.src = SurvivorImg[index];
+
+}
+
+async function spin() {
+    spinning = !spinning
+    console.log(spinning)
+    
+    var timer = setInterval(changeSurvivorImage, 40)
+    var perkTimer = setInterval(changePerkImage)
+    
+    await new Promise(r => setTimeout(r, 2000));
+    clearInterval(timer);
+    clearInterval(perkTimer);
+}
+
+function changePerkImage(perk) {
+    var index1 = Math.floor(Math.random()*(SurvivorPerkImg.length-1 - 0 + 1) + 0)
+    var index2 = Math.floor(Math.random()*(SurvivorPerkImg.length-1 - 0 + 1) + 0)
+    while( index2 == index1)
+    {
+        var index2 = Math.floor(Math.random()*(SurvivorPerkImg.length-1 - 0 + 1) + 0)
+    }
+    var index3 = Math.floor(Math.random()*(SurvivorPerkImg.length-1 - 0 + 1) + 0)
+    while (index3 == index2 || index3 == index1)
+    {
+        var index3 = Math.floor(Math.random()*(SurvivorPerkImg.length-1 - 0 + 1) + 0)
+    }
+    var index4 = Math.floor(Math.random()*(SurvivorPerkImg.length-1 - 0 + 1) + 0)
+    while (index4 == index2 || index4 == index2 || index4 == index1)
+    {
+        var index4 = Math.floor(Math.random()*(SurvivorPerkImg.length-1 - 0 + 1) + 0)
+    }
+    perk1.src = SurvivorPerkImg[index1];
+    perk1text.innerHTML = SurvivorPerkImg[index1].substring(16, SurvivorPerkImg[index1].length - 5);
+    perk2.src = SurvivorPerkImg[index2];
+    perk2text.innerHTML = SurvivorPerkImg[index2].substring(16, SurvivorPerkImg[index2].length - 5);
+    perk3.src = SurvivorPerkImg[index3];
+    perk3text.innerHTML = SurvivorPerkImg[index3].substring(16, SurvivorPerkImg[index3].length - 5);
+    perk4.src = SurvivorPerkImg[index4];
+    perk4text.innerHTML = SurvivorPerkImg[index4].substring(16, SurvivorPerkImg[index4].length - 5);
+}
